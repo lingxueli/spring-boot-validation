83851f65c50c9193877f725fd48d36e61f97ebb9
lingxueli <ll2871@columbia.edu>
2019-08-11 00:52:34 -0600

    fix bean

--- src/main/java/com/example/springbootvalidation/model/User.java
+++ src/main/java/com/example/springbootvalidation/model/User.java
@@ -14,4 +14,24 @@ public class User {
         this.password = password;
         this.grade = grade;
     }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public Double getGrade(){
+        return this.grade;
+    }
+    public void setGraded(Double grade){
+        this.grade = grade;
+    }
+
 }
